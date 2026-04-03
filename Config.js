1ed436225e7b32cc5b3c3b5411a8d50cff0a18a3
robin-pixel-del <juanrtrt26@gmail.com>
2026-04-03 17:10:35 -0500

    Config.js

--- Config.js
+++ Config.js
@@ -1,7 +1,7 @@
 (function() {
     window.SISTEMA_VIP = {
         "status": "ACTIVO",
-        "agente": "Winamp_VIP_Mobile_2026",
+        "User-Agent": "Winamp_VIP_Mobile_2026",
         "worker": "https://jolly-fire-1228basuuiiira.juanrtrt200.workers.dev/?key=forever221";
 
     };
