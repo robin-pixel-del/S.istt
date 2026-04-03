6fcc06ac9e117380717532f8e3d1596fd9d21881
robin-pixel-del <juanrtrt26@gmail.com>
2026-04-03 17:12:48 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,6 +3,5 @@
         "status": "ACTIVO",
         "User-Agent": "Winamp_VIP_Mobile_2026",
         "worker": "https://jolly-fire-1228basuuiiira.juanrtrt200.workers.dev/?key=forever221";
-
     };
 })();
