8d1d4d8df73d9bdce7e430e190e087d30191365d
robin-pixel-del <juanrtrt26@gmail.com>
2026-04-03 22:28:14 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 (function() {
     window.SISTEMA_VIP = {
         "status": "ACTIVO",
-        "User-Agent": "Winamp_VIP_Mobile_2026",
-        "worker": "https://polished-queen-67c2erescabro.juanrtrt200.workers.dev/";
-       ";
+        "key": "forever221", 
+        "agente": "Winamp_VIP_Mobile_2026",
+        "worker": "https://polished-queen-67c2erescabro.juanrtrt200.workers.dev/"
     };
 })();
