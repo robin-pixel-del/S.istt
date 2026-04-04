62b9b78d52fd1b360e9ded540a19d7f7727171d2
robin-pixel-del <juanrtrt26@gmail.com>
2026-04-03 22:06:40 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,7 @@
     window.SISTEMA_VIP = {
         "status": "ACTIVO",
         "User-Agent": "Winamp_VIP_Mobile_2026",
-        "worker": "https://spring-cloud-e1b1hijodetuma.juanrtrt200.workers.dev/";
+        "worker": "https://polished-queen-67c2erescabro.juanrtrt200.workers.dev/";
+       ";
     };
 })();
